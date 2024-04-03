15d0687360d40fbff7d0f5a24c01bb2397bec74e
= <=fontalvo0218@gmail.com>
2024-04-02 22:53:48 -0500

    first commit

---
+++ src/test/java/com/unimagdalena/productStore/ProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.productStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
