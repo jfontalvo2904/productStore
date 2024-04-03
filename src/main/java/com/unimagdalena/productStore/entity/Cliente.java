f70a7424b4818598bbe72eb7de2756f5d4d6d243
= <=fontalvo0218@gmail.com>
2024-04-03 10:08:50 -0500

    cambio en la entidad cliente

--- src/main/java/com/unimagdalena/productStore/entity/Cliente.java
+++ src/main/java/com/unimagdalena/productStore/entity/Cliente.java
@@ -18,8 +18,13 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column( nullable = false)
     private String nombre;
+
+    @Column(nullable = false, unique = true)
     private String email;
+    
     private String direccion;
 
 
--- src/main/java/com/unimagdalena/productStore/repository/ClienteRepository.java
+++ src/main/java/com/unimagdalena/productStore/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.unimagdalena.productStore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.unimagdalena.productStore.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Query("S")
+    Cliente buscarClientePorEmail(String email);
     
 }
