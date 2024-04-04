590ff17df28ca1a5f423cdd8365902a6989ce554
= <=fontalvo0218@gmail.com>
2024-04-04 17:11:12 -0500

    cliente controller

--- src/main/java/com/unimagdalena/productStore/services/cliente/ClienteService.java
+++ src/main/java/com/unimagdalena/productStore/services/cliente/ClienteService.java
@@ -1,4 +1,24 @@
 package com.unimagdalena.productStore.services.cliente;
 
+import com.unimagdalena.productStore.dto.cliente.ClienteDto;
+import com.unimagdalena.productStore.dto.cliente.ClienteToSaveDto;
+import com.unimagdalena.productStore.dto.cliente.ClienteToUpdateDto;
+import com.unimagdalena.productStore.entity.Cliente;
+import com.unimagdalena.productStore.exception.cliente.ClienteNotFoundException;
+
+import java.util.List;
+
 public interface ClienteService {
+
+
+    List<ClienteDto> obtenerTodos();
+    ClienteDto crearCliente(ClienteToSaveDto clienteToSaveDto);
+    ClienteDto actualizarCliente(Long id, ClienteToUpdateDto clienteToUpdateDto) throws ClienteNotFoundException;
+    ClienteDto buscarClientePorId(Long id) throws ClienteNotFoundException;
+    Cliente buscarCliente(Long id) throws ClienteNotFoundException;
+    void eliminarCliente(Long id) throws ClienteNotFoundException;
+    ClienteDto buscarClientePorEmail(String email) throws ClienteNotFoundException;
+    List<ClienteDto> buscarClientePorDireccion( String direccion);
+    List<ClienteDto> buscarClientePorNombre(String nombre);
+
 }
