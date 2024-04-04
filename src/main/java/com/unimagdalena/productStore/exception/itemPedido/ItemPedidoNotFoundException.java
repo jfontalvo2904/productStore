0f2a8c712b8a151c7d84e95b38f4a3040c830398
= <=fontalvo0218@gmail.com>
2024-04-04 08:53:49 -0500

    crando exceptions

---
+++ src/main/java/com/unimagdalena/productStore/exception/itemPedido/ItemPedidoNotFoundException.java
@@ -0,0 +1,23 @@
+package com.unimagdalena.productStore.exception.itemPedido;
+
+public class ItemPedidoNotFoundException extends RuntimeException{
+    public ItemPedidoNotFoundException() {
+        super("El item de pedido no ha sido encontrado");
+    }
+
+    public ItemPedidoNotFoundException(String message) {
+        super(message);
+    }
+
+    public ItemPedidoNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ItemPedidoNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ItemPedidoNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
