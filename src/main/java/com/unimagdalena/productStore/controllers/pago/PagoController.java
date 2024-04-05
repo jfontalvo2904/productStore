339584f6111014de2ba8d60736fb6b56c212a317
= <=fontalvo0218@gmail.com>
2024-04-04 23:31:58 -0500

    url para pago

--- src/main/java/com/unimagdalena/productStore/controllers/pago/PagoController.java
+++ src/main/java/com/unimagdalena/productStore/controllers/pago/PagoController.java
@@ -2,11 +2,14 @@ package com.unimagdalena.productStore.controllers.pago;
 
 import com.unimagdalena.productStore.dto.pago.PagoDto;
 import com.unimagdalena.productStore.dto.pago.PagoToSaveDto;
+import com.unimagdalena.productStore.dto.pago.PagoToUpdateDto;
 import com.unimagdalena.productStore.services.pago.PagoServiceImpl;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,4 +32,21 @@ public class PagoController {
     ResponseEntity<PagoDto> crearPago(@RequestBody PagoToSaveDto pagoToSaveDto) {
         return ResponseEntity.ok().body(this.pagoService.crearPago(pagoToSaveDto));
     }
+
+    @PutMapping("/{pagoId}")
+    ResponseEntity<PagoDto> actualizarPago(@PathVariable("pagoId") Long pagoId, @RequestBody PagoToUpdateDto pagoToUpdateDto){
+        return ResponseEntity.ok().body(this.pagoService.actualizarPago(pagoId, pagoToUpdateDto));
+    }
+
+    @GetMapping("/{pedidoId}")
+    ResponseEntity<PagoDto> buscarPagoPorId( @PathVariable("pedidoId") Long id) {
+        return ResponseEntity.ok().body(this.pagoService.buscarPagoPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> eliminarPago(@PathVariable("id") Long id) {
+        this.pagoService.eliminarPago(id);
+        return ResponseEntity.ok().body("Pago eliminado correctamente");
+    }
+
 }
